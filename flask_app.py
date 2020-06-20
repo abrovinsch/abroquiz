8b34794ed531d609a351723c8375aeace168f605
Oskar Lundqvist <lundqvist.oskar01@gmail.com>
2020-06-20 16:21:21 +0200

    Dummy site

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='/home/abrovinsch/abroquiz/templates')
+app = Flask(__name__)
+
+# Landing page
+@app.route('/')
+def landingpage():
+    with app.app_context():
+        return render_template("home.html")
 
 # Edit interface
 @app.route('/abroquiz/edit')
