c4e319af9bbd29e6a88d9dd9911136600fd62057
Oskar Lundqvist <lundqvist.oskar01@gmail.com>
2020-06-21 12:51:38 +0200

    Use Table for questions instead

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,43 +1,64 @@
-const questionList = document.getElementById("questionlist")
+const tableBody = document.getElementById("table-body")
 
 async function GetQuestions(id, topic) {
+   RemoveOldQuestions();
+
    const response = await fetch(`/questions/${id}/${topic}`);
    const result = await response.json();
 
    questionList.textContent = "";
 
-   for(let item of result) {
+   for (let item of result) {
       AddQuestionToList(item);
    }
 
 }
 
+function NewCell(text) {
+   let cell = document.createElement("td");
+   cell.textContent = text;
+   return cell;
+}
+
+function RemoveOldQuestions() {
+   var elements = document.getElementsByClassName('question-row');
+
+   while (elements[0]) {
+      elements[0].parentNode.removeChild(elements[0]);
+   }
+}
+
 function AddQuestionToList(item) {
-   let listNode = document.createElement("LI")
-   let textNode = document.createTextNode(`${item.question} = ${item.ans1}`);
-   listNode.appendChild(textNode);
-   questionlist.appendChild(listNode);
+   let rowNode = document.createElement("tr")
+   rowNode.className = "question-row";
+
+   rowNode.appendChild(NewCell(`${item.question_id}`));
+   rowNode.appendChild(NewCell(`${item.question}`));
+   rowNode.appendChild(NewCell(`${item.ans1}`));
+   rowNode.appendChild(NewCell(`${item.topic}`));
+
+   tableBody.appendChild(rowNode);
 }
 
 function ClickButton() {
    questionList.textContent = "loading";
-   GetQuestions(0, "båt");
+   GetQuestions(0, "Marin");
 }
 
 async function SubmitNewQuestion() {
 
    let form = {
-     question: document.getElementById('question').value,
-     answer: document.getElementById('answer').value,
-     topic: document.getElementById('topic').value
-  };
+      question: document.getElementById('question').value,
+      answer: document.getElementById('answer').value,
+      topic: document.getElementById('topic').value
+   };
 
    const result = await fetch('/abroquiz/submit', {
-     method: 'POST',
-     headers: {
-      'Content-Type': 'application/json'
-    },
-     body: JSON.stringify(form)
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
    });
    console.log(result);
 }
